1dce77e49bd43a6f7cd2377c02723848e51e23b8
HeatCraB <heatcrab999@gmail.com>
2025-03-09 12:43:10 +0800

    Update four newly functions for queue operations
    Implement deletion of middle node using the fast-slow pointer
    technique to locate the middle, enhancing flexibility in
    boundary checks. For duplicate string removal, leverage the
    existing header function to check if the queue has a single
    node, speeding up the process. Use a safe traversal method
    to identify and remove duplicates, preventing memory issues.
    Add functions to remove the first and last nodes, utilizing
    functions defined in the header to accelerate operations,
    simplify code, and improve readability. Employ C library
    functions to handle string duplication tasks efficiently.
    
    Change-Id: I156a6c1741fd5d41b1d64d47ebbced9ebe27688a

--- queue.c
+++ queue.c
@@ -67,13 +67,35 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+
+    element_t *e = list_first_entry(head, element_t, list);
+    list_del(&e->list);
+
+    if (sp && bufsize > 0) {
+        strncpy(sp, e->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    return e;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+
+    element_t *e = list_last_entry(head, element_t, list);
+    list_del(&e->list);
+
+    if (sp && bufsize > 0) {
+        strncpy(sp, e->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    return e;
 }
 
 /* Return number of elements in queue */
@@ -91,17 +113,58 @@ int q_size(struct list_head *head)
     return len;
 }
 
-/* Delete the middle node in queue */
 bool q_delete_mid(struct list_head *head)
 {
     // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+
+    if (!head || list_empty(head))
+        return false;
+
+    if (list_is_singular(head)) {
+        element_t *elem = list_first_entry(head, element_t, list);
+        list_del(&elem->list);
+        q_release_element(elem);
+        return true;
+    }
+
+    struct list_head *slow = head->next;
+    struct list_head *fast = head->next;
+
+    while (fast != head && fast->next != head) {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+
+    element_t *mid = list_entry(slow, element_t, list);
+    list_del(slow);
+    q_release_element(mid);
+
     return true;
 }
 
-/* Delete all nodes that have duplicate string */
 bool q_delete_dup(struct list_head *head)
 {
     // https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+
+    if (!head || list_empty(head))
+        return false;
+
+    if (list_is_singular(head))
+        return true;
+
+    struct list_head *node, *safe;
+    const element_t *prev = NULL;
+
+    list_for_each_safe (node, safe, head) {
+        element_t *curr = list_entry(node, element_t, list);
+        if (prev && strcmp(prev->value, curr->value) == 0) {
+            list_del(&curr->list);
+            q_release_element(curr);
+        } else {
+            prev = curr;
+        }
+    }
+
     return true;
 }
 
